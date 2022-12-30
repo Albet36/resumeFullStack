98196ca018c99aa690a9caa10b2980c8361efcd8
Your Name <you@example.com>
2022-12-30 12:31:44 +0700

    updated

--- front-end/src/api/data.js
+++ front-end/src/api/data.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const dataAll =  async() => {
     try {
-        return await axios.get(process.env.API_BACK_END); 
+        return await axios.get('http://localhost:2001/listData'); 
     } catch (error) {
         console.log(error);
     }
