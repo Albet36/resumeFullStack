1604a2c21f86576b850c65ed45971daa0becbdc4
Your Name <you@example.com>
2023-01-09 20:17:00 +0700

    updated

--- back-end/src/api/api.js
+++ back-end/src/api/api.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { emailSend } from '../Me/Controllers/Email.controllers.js';
 import { dataAll } from '../Me/Controllers/Me.controllers.js';
 
 
 const api = express.Router();
 api.get('/listData',dataAll);
-api.post('/sendForm',emailSend);
 
 export default api;
